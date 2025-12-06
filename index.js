9c516a1df6b2e963004a8325971b45b0934f4df7
JHR <iamjhrakib@gmail.com>
2025-12-06 19:01:09 +0600

    added necessary packages

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
